4afb3326964b7e0c3c77a4dfec8c4f3523d634d1
Artimum <dorghamlli12@gmail.com>
2023-09-07 16:08:21 +0300

    more effecient

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import app from "./app.css"
+import "./app.css"
 import Nav from "./nav"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
